daf64f47dc55c9117ef13310a7ee6610ad1d167b
Robbie Symonds <robbiesymonds26@gmail.com>
2019-03-17 20:57:04 +1030

    Added Canvas Page Tab

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # New-Tab Pages
 
 ## Keystone
-Provides access to frequent websites. Includes integration with Keystone, homework and favourites are accessible from a new tab. This can be downloaded and loaded in an unpackaged from  `keystone` or from the [Chrome Web Store](https://chrome.google.com/webstore/detail/keystone-tab-page/jcneddonepgmnmjallnnkecmlndhlbcp)
+Provides access to frequent websites. Includes integration with Keystone, homework and favourites are accessible from a new tab. This can be downloaded and loaded in an unpackaged from `keystone` or from the [Chrome Web Store](https://chrome.google.com/webstore/detail/keystone-tab-page/jcneddonepgmnmjallnnkecmlndhlbcp)
 
-## Adelaide University
-The new tab page successor to the **Keystone Tab Page**. It provides access to a set of customisable websites as well as functionality to automatically open multiple pages at once by press `TAB` twice (Use `~` to edit the list). Includes integration with University of Adelaide platforms for timetable, assignments, and more. This can be downloaded and loaded in an unpackaged from  `university` or from the [Chrome Web Store](#)
+## Canvas
+The new tab page successor to the **Keystone Tab Page**. It provides access to a set of customisable websites as well as functionality to automatically open multiple pages at once by press `TAB` twice (Use `~` to edit the list). Includes integration with the Canvas platform for courses, assignments, and more. (A common platform choice for universities) This can be downloaded and loaded in an unpackaged from `canvas` or from the [Chrome Web Store](#)
---
+++ canvas/assets/js/script.js
@@ -0,0 +1,242 @@
+var tasks;
+var token;
+var names = [];
+var quickLoad_state = false;
+document.onkeydown = quickLoad;
+var currentMoment = moment();
+
+function getAccessToken() {
+	$('.token-fade').fadeToggle(200);
+	setTimeout(function() {
+		$('.token-box').toggleClass('token-hide');
+	}, 200);
+}
+
+$('#token_button').click(function() {
+	if ($('.token-text').val().length > 1) {
+		chrome.storage.local.set({'access_token': $('.token-text').val().trim()});
+		$('.token-fade').fadeToggle(200);
+		setTimeout(function() {
+			$('.token-box').toggleClass('token-hide');
+		}, 200);
+	}
+});
+
+
+chrome.storage.local.get('quickLoad_state', function(result) {
+   	quickLoad_state = result.quickLoad_state;
+   	$('.quickload-text').val(quickLoad_state);
+});
+
+function quickLoad(e) {
+	var e = window.event;
+	if (e.keyCode == 9) {
+		chrome.storage.local.get('quickLoad_state', function(result) {
+			if (result.quickLoad_state) {
+			   	quickLoad_state = result.quickLoad_state;
+			   	var quickLoad_urls = quickLoad_state.replace(", ", ",");
+			   	var url_array = quickLoad_urls.split(',');
+			   	var url_first = true;
+			   	$.each(url_array, function(key) {
+			   		if (url_first == true) {
+			   			window.location.href = url_array[key];
+			   			url_first = false;
+			   		} else {
+			   			chrome.tabs.create({url: url_array[key]});
+			   		}
+			   	});
+		   	} else {
+		   		$('.fullscreen-fade').fadeToggle(200);
+				setTimeout(function() {
+					$('.quickload-box').toggleClass('quickload-hide');
+				}, 200);
+		   	}
+		});
+	}
+
+	if (e.keyCode == 192) {
+		$('.fullscreen-fade').fadeToggle(200);
+		setTimeout(function() {
+			$('.quickload-box').toggleClass('quickload-hide');
+		}, 200);
+	}
+}
+
+$('#quickload_button').click(function() {
+	if ($('.quickload-text').val().length > 1) {
+		chrome.storage.local.set({'quickLoad_state': $('.quickload-text').val()});
+		$('.fullscreen-fade').fadeToggle(200);
+		setTimeout(function() {
+			$('.quickload-box').toggleClass('quickload-hide');
+		}, 200);
+	} else {
+		$('.fullscreen-fade').fadeToggle(200);
+		setTimeout(function() {
+			$('.quickload-box').toggleClass('quickload-hide');
+		}, 200);
+	}
+});
+
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
+$('.favourites').click(function() {
+	$('.favourites').fadeOut(200);
+	$('.favourites-box').addClass('box-hidden');
+	$('.favourites-link').addClass('link-hidden');
+});
+
+$('.favourites').on('mousewheel', function(e) {
+    var delta = e.originalEvent.wheelDelta;
+
+    if (delta > 0) {
+        $('.favourites').fadeOut(200);
+		$('.favourites-box').addClass('box-hidden');
+		$('.favourites-link').addClass('link-hidden');
+    }
+});
+
+$('.fav-button').click(function(e) {
+	e.preventDefault();
+	$('.favourites').fadeIn(200);
+	$('.favourites-box').removeClass('box-hidden');
+	setTimeout(function() {
+		$('.favourites-link').removeClass('link-hidden');
+	}, 100);
+});
+
+chrome.storage.local.get('access_token', function(result) {
+   if (!result.access_token) {
+   		getAccessToken();
+   } else {
+   	   	token = result.access_token;
+		$.getJSON('https://myuni.adelaide.edu.au/api/v1/courses?access_token'+token, function(courses) {
+			for (var i = 0; i < courses.length; i++) {
+				var course = courses[i];		
+				var id = course.id;
+				names.push({'id': id, 'name': course.name});
+				$item = '<a style="color: black;" href="https://myuni.adelaide.edu.au/courses/'+id+'"><div class="favourite"><h1 class="favourites-title">'+course.name+' <span class="pin">'+id+'</span></h1></div></a>';
+				$('.favourites-box').append($item);
+			}
+			currentDate = moment().format('YYYY-MM-DD');
+		   	tomorrowDate = relativeDate(+1);
+	   		loadTasks(currentDate, tomorrowDate);
+		});
+	}
+});
+
+function relativeDate(change) {
+	return moment().hours(change*24).format('YYYY-MM-DD');
+}
+
+function loadTasks(start, end) {
+	$.getJSON('https://myuni.adelaide.edu.au/api/v1/planner/items?start_date='+start+'&end_date='+end+'&per_page=100&access_token='+token, function(data) {
+		var dayOfMonth = moment(currentMoment).date();
+		var today = moment().date();
+		if (dayOfMonth == today){
+			$('.day').html('Today');
+		} else if(dayOfMonth == today + 1){
+			$('.day').html('Tomorrow');
+		} else if(dayOfMonth == today - 1){
+			$('.day').html('Yesterday');
+		} else {
+			$('.day').html(moment(currentMoment).format("dddd"));
+		}
+		$('.date-full').html(moment(currentMoment).format("Do of MMMM, YYYY"));
+		$('.homework-body').html('');
+		for (var i = 0; i <= data.length-1; i++) {
+			addTask(data[i]);
+		}
+	});
+}
+
+function addTask(task) {
+	var course;
+	var title = task.plannable.title;
+	var type = task.plannable_type;
+
+	for (i = 0; i <= names.length-1; i++) {
+		if (task.course_id == names[i].id)
+			var course = names[i].name;
+	}
+
+	var status;
+	if (task.plannable_type == 'quiz') {
+		if (task.planner_override == null)
+			status = task.submissions.submitted;
+		else
+			status = task.planner_override.marked_complete;
+	} else {
+		if (task.planner_override == null)
+			status = false;
+		else
+			status = task.planner_override.marked_complete;
+	}
+
+	checkedClass = (status == true) ? 'checked' : '';
+	checkedHTML = (status == true) ? '<svg style="width:32px;height:32px" viewBox="0 0 24 24"><path fill="grey" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" /></svg>' : '';
+	$item = '<div id="'+task.plannable_id+'" class="homework-item"><div class="homework-checkbox '+checkedClass+'">'+checkedHTML+'</div><div class="homework-info"><a href="https://myuni.adelaide.edu.au'+task.html_url+'"><h1 class="homework-title">'+title+'</h1></a><h3 class="homework-details">'+task.plannable_type+' <span class="homework-subject">'+course+'</span></h3></div>';
+	$('.homework-body').append($item);
+}
+
+$(document).ready(function() {
+	setTimeout(function() {
+		if ($('.homework-body').html() == '') {
+			$('.homework-body').html('<div class="no-login"><div class="no-login-icon"><svg style="width:100px;height:100px" viewBox="0 0 24 24"><path fill="#F44336" d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg></div><h1 class="no-login-text">Unable to connect.</h1></div></div>');
+		}
+
+		if ($('.favourites-box').html() == '') {
+			$('.favourites-box').html('<div class="no-login"><div class="no-login-icon"><svg style="width:100px;height:100px" viewBox="0 0 24 24"><path fill="#F44336" d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg></div><h1 class="no-login-text">Unable to connect.</h1></div></div>');
+		}
+	}, 3000);
+});
+
+$('.left-arrow').click(function() {
+	currentMoment = moment(currentMoment).hours(-24).format('YYYY-MM-DD');
+	nextMoment = moment(currentMoment).hours(24).format('YYYY-MM-DD');
+	loadTasks(currentMoment, nextMoment);
+});
+
+$('.right-arrow').click(function() {
+	currentMoment = moment(currentMoment).hours(24).format('YYYY-MM-DD');
+	nextMoment = moment(currentMoment).hours(24).format('YYYY-MM-DD');
+	nextMoment = moment(currentMoment).hours(24).format('YYYY-MM-DD');
+	loadTasks(currentMoment, nextMoment);
+});
+
+function showHomeworkTile() {
+	$('.items').addClass('hidden-top');
+	$('.homework-panel').removeClass('homework-hidden');
+	$('.up-arrow').show();
+	setTimeout(function() {
+		$('.up-arrow').removeClass('up-arrow-hidden');
+	}, 300);
+}
+
+function hideHomeworkTile() {
+	$('.items').removeClass('hidden-top');
+		$('.homework-panel').addClass('homework-hidden');
+		$('.up-arrow').addClass('up-arrow-hidden');
+		setTimeout(function() {
+			$('.up-arrow').hide();
+	}, 100);
+}
+
+$('.up-arrow').click(function() {
+	window.location.href = "https://myuni.adelaide.edu.au/";
+});
+
+$('.wrapper').on('mousewheel', function(e) {
+    var delta = e.originalEvent.wheelDelta;
+    if (delta < 0) {
+        showHomeworkTile();
+    } else if (delta > 0) {
+    	if ($('.homework-body')[0].scrollHeight > $('.homework-panel')[0].scrollHeight) {
+    		if ($(e.target).parents('.homework-body').length <= 0)
+    			hideHomeworkTile();
+		} else {
+			hideHomeworkTile();
+		}
+    }
+});
